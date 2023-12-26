431d732e52262b7d66939c87b4293b451bd2e05c
prcryx <me.prc.fo@gmail.com>
2023-12-27 00:13:43 +0530

    improve comments

--- lib/widget.dart
+++ lib/widget.dart
@@ -2,13 +2,16 @@ part of flutter_responsive_layout;
 
 typedef ResponsiveLayoutBuilder = Widget Function(BuildContext context);
 
+/// A Flutter widget that facilitates building responsive layouts.
+///
 /// Add the [ResponsiveLayout] to your [MaterialApp] or [CupertinoApp]
+/// The `ResponsiveLayout` widget takes a [ResponsiveLayoutBuilder] function,
+/// which is responsible for constructing the UI based on the current screen size
+/// and orientation.
+/// designSize is used for UI width and height. defaultSize: Size(360, 800)
 ///
 class ResponsiveLayout extends StatelessWidget {
   final ResponsiveLayoutBuilder builder;
-
-  /// UI width and height. defaultSize: Size(360, 800)
-  ///
   final Size? designSize;
 
   const ResponsiveLayout({
