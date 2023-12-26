451713c3ad0bda08cd068f2b64adbb6fbca8c0f2
prcryx <me.prc.fo@gmail.com>
2023-12-27 02:06:07 +0530

    comments

--- lib/screen_extension.dart
+++ lib/screen_extension.dart
@@ -1,6 +1,13 @@
 part of flutter_responsive_layout;
 
+/// Adapted to the device width as per the UI Design
+///
 extension ScreenExtension on num {
+  /// Adapted Height
+  ///
   double get h => this * ScreenUtils().sh;
+
+  /// Adapted Width
+  ///
   double get w => this * ScreenUtils().sw;
 }
--- lib/widget_extension.dart
+++ lib/widget_extension.dart
@@ -1,6 +1,10 @@
 part of flutter_responsive_layout;
 
 extension WidgetExtension on num {
+  /// SizedBox with adapted height
+  /// 
   SizedBox get hs => SizedBox(height: this * ScreenUtils().sh);
+   /// SizedBox with adapted width
+  /// 
   SizedBox get ws => SizedBox(width: this * ScreenUtils().sw);
 }
