455f868bbce0ef86eb8edc6bc45491e820fbfc52
prcryx <me.prc.fo@gmail.com>
2023-12-28 00:02:00 +0530

    comments

--- lib/widget_extension.dart
+++ lib/widget_extension.dart
@@ -1,10 +1,11 @@
 part of flutter_responsive_layout;
 
 extension WidgetExtension on num {
-  /// SizedBox with adapted height
-  /// 
+  /// SizedBox with adaptive height
+  ///
   SizedBox get hs => SizedBox(height: this * ScreenUtils().sh);
-   /// SizedBox with adapted width
-  /// 
+
+  /// SizedBox with adaptive width
+  ///
   SizedBox get ws => SizedBox(width: this * ScreenUtils().sw);
 }
